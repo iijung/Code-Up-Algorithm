b4dc2cd22b9f55ac2c145e61af99bb42ae74ce7d
iijung <987kmj4@naver.com>
2019-10-08 21:17:01 +0900

    1068

---
+++ 기초 100제/1068 [기초-조건,선택실행구조] 정수 1개 입력받아 평가 출력하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int score = 0;
+    scanf("%d", &score);
+    if (score >= 90)
+        printf("A");
+    else if (score >= 70)
+        printf("B");
+    else if (score >= 40)
+        printf("C");
+    else
+        printf("D");
+    return 0;
+}
