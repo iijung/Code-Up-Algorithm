a87b20fb484576cf13702f669ccad6073d9c2f5b
iijung <987kmj4@naver.com>
2019-10-09 20:26:54 +0900

    1081

---
+++ 기초 100제/1081 [기초-종합] 주사위를 2개 던지면.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int dice1 = 0, dice2 = 0;
+    scanf("%d %d", &dice1, &dice2);
+    for (int i = 1; i <= dice1; i++)
+    {
+        for (int j = 1; j <= dice2; j++)
+        {
+            printf("%d %d\n", i, j);
+        }
+    }
+    return 0;
+}
