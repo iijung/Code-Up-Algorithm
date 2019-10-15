f67b7c3368b6ac08d832c6bde11f66d9b75a1531
iijung <987kmj4@naver.com>
2019-10-15 20:50:16 +0900

    1116 : 사칙연산 계산기

---
+++ 1101 ~ 1200/1116.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 0, num2 = 0;
+    scanf("%d %d", &num1, &num2);
+    printf("%d+%d=%d\n", num1, num2, num1 + num2);
+    printf("%d-%d=%d\n", num1, num2, num1 - num2);
+    printf("%d*%d=%d\n", num1, num2, num1 * num2);
+    printf("%d/%d=%d\n", num1, num2, num1 / num2);
+    return 0;
+}
