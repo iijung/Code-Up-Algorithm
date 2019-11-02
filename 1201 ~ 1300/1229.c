2365757fcc8c1ee91ac83f7be2ed5f6513d750e7
iijung <987kmj4@naver.com>
2019-11-03 00:35:04 +0900

    1229 : 비만도 측정 2

---
+++ 1201 ~ 1300/1229.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    double height = 0, weight = 0;
+    scanf("%lf %lf", &height, &weight);
+
+    double avg = 0;
+    if (height < 150)
+    {
+        avg = (height - 100);
+    }
+    else if (height < 160)
+    {
+        avg = (height - 150) / 2 + 50;
+    }
+    else
+    {
+        avg = (height - 100) * 0.9;
+    }
+
+    double bmi = (weight - avg) * 100 / avg;
+
+    printf("%s", bmi > 20 ? "비만" : bmi > 10 ? "과체중" : "정상");
+
+    return 0;
+}
