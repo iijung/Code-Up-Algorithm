dd8b87cd519bf4e906ccaabe5283948a731b129b
iijung <987kmj4@naver.com>
2019-11-26 00:26:01 +0900

    1356 : 사각형 출력하기 2

---
+++ 1301 ~ 1400/1356.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%c", i == 0 || i == n - 1 || j == 0 || j == n - 1 ? '*' : ' ');
+        }
+        printf("\n");
+    }
+    return 0;
+}
