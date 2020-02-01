e0a8a219c153f658d39643b372581b878f8ef4ac
iijung <987kmj4@naver.com>
2020-02-02 00:07:05 +0900

    1469 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-2

---
+++ 1401 ~ 1500/1469.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", i % 2 ? n * i + 1 + j : 2 * n * (i + 1) / 2 - j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
