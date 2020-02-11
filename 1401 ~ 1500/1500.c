2aa46eea4086fbeddb51cbaabcc788b36838f59d
iijung <987kmj4@naver.com>
2020-02-11 23:42:21 +0900

    1500 : 2차원 배열에 값 저장하기

---
+++ 1401 ~ 1500/1500.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0, m = 0;
+    scanf("%d %d", &n, &m);
+
+    int arr[100][100] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
