b91053045f5e261cd03c9a99e554a628879a1fb4
iijung <987kmj4@naver.com>
2020-02-12 21:38:58 +0900

    1476 : [기초-배열연습] 2차원 배열 빗금 채우기 3-1

---
+++ 1401 ~ 1500/1476.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100][100] = {0};
+
+    int n = 0, m = 0;
+    scanf("%d %d", &n, &m);
+
+    int cnt = 1;
+    int dot_i = 1, dot_j = 1;
+    while (cnt <= n * m)
+    {
+        int i = dot_i - 1, j = dot_j - 1;
+        while (i >= 0 && j < m)
+        {
+            arr[i--][j++] = cnt++;
+        }
+        dot_i < n ? dot_i++ : dot_j++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
