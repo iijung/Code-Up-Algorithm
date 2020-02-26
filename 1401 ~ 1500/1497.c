530461cce736447077cd30be6ad32b1fb401c5e0
iijung <987kmj4@naver.com>
2020-02-26 21:53:42 +0900

    1497 : [기초-배열연습] 두 개씩 묶어 큰 값 골라 배열 만들기 5-6

---
+++ 1401 ~ 1500/1497.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    int arr[100] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int result[100] = {0};
+    for (int i = 0; i < n; i += 2)
+    {
+        result[i / 2] = arr[i] > arr[i + 1] ? arr[i] : arr[i + 1];
+    }
+
+    for (int i = 0; i < n / 2; i++)
+    {
+        printf("%d ", result[i]);
+    }
+
+    return 0;
+}
