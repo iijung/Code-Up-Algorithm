9d0e5b84a21d2235ebae2f0c6432d50fcb67ee3f
iijung <987kmj4@naver.com>
2020-03-03 22:56:16 +0900

    1506 : 2차원 배열 채우기 4(역달팽이 배열)

---
+++ 1501 ~ 1600/1506.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int arr[50][50];
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+
+    int cnt = 1;
+    int round = n;
+    int i = -1, j = 0;
+    while (round > 0)
+    {
+        for (int idx = 0; idx < round; idx++)
+        {
+            arr[++i][j] = cnt++;
+        }
+        round--;
+        for (int idx = 0; idx < round; idx++)
+        {
+            arr[i][++j] = cnt++;
+        }
+        for (int idx = 0; idx < round; idx++)
+        {
+            arr[--i][j] = cnt++;
+        }
+        --round;
+        for (int idx = 0; idx < round; idx++)
+        {
+            arr[i][--j] = cnt++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
