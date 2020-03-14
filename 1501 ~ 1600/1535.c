b4d929aa00b233959e5c4d134c65d88b4ab94efe
iijung <987kmj4@naver.com>
2020-03-14 22:22:09 +0900

    1535 : [기초-함수작성] 함수로 가장 큰 값 위치 리턴하기

---
+++ 1501 ~ 1600/1535.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int n, d[110];
+
+int f()
+{
+    int max = d[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (max < d[i])
+        {
+            max = d[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (max == d[i])
+        {
+            return i + 1;
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &d[i]);
+
+    printf("%d", f());
+    return 0;
+}
