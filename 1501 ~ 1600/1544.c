3f7e5b3df49753c606206cd60ad58c55cb639f8c
iijung <987kmj4@naver.com>
2020-03-16 21:16:48 +0900

    1544 : [기초-함수작성] 함수로 * n개 출력하기

---
+++ 1501 ~ 1600/1544.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int n;
+
+void f(int a)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("*");
+    }
+}
+
+int main()
+{
+    scanf("%d", &n);
+    f(n);
+    return 0;
+}
