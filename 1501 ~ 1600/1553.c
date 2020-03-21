f1a57767d3425c343377bafe559d6c82bf0c54d0
iijung <987kmj4@naver.com>
2020-03-21 18:35:23 +0900

    1553 : [기초-함수작성] 함수로 정수 올림 한 값 리턴하기

---
+++ 1501 ~ 1600/1553.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+double x;
+
+long long int f(double a)
+{
+    long long int t = a;
+    return a - t > 0 ? t + 1 : t;
+}
+
+int main()
+{
+    scanf("%lf", &x);
+    printf("%lld\n", f(x));
+}
