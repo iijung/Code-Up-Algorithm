6cbf7e421556ca7c13c8b545e7c928cb42889a7f
iijung <987kmj4@naver.com>
2020-04-20 21:46:57 +0900

    1661 : 파싱(parsing) 2

---
+++ 1601 ~ 1700/1661.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100] = {0};
+    gets(str);
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == ',')
+        {
+            printf(" ");
+        }
+        else if (str[i] == ' ')
+        {
+            continue;
+        }
+        else if (str[i] == ';')
+        {
+            printf(" \n");
+        }
+        else
+        {
+            printf("%c", str[i]);
+        }
+    }
+    printf(" ");
+
+    return 0;
+}
