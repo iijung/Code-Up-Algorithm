660d506f6c4ca2e8ab1cf519c5d5d3d99d7e43e4
iijung <987kmj4@naver.com>
2020-05-09 22:55:12 +0900

    1860 : [기초-재귀함수] 수 삼각형 출력하기

---
+++ 1801 ~ 1900/1860.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void g(int k)
+{
+    if (k < 1)
+        return;
+    g(k - 1);
+    printf("%d ", k);
+}
+
+void f(int k)
+{
+    if (k < 1)
+        return;
+    f(k - 1);
+    g(k);
+    printf("\n");
+}
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    f(n);
+}
